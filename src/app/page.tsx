833d9c10aa7abc6cb93d82ee9a8024756ac3bb75
Marqui <bmay86@protonmail.com>
2026-05-21 21:47:19 -0400

    feat: integrate Clerk authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { SignInButton, UserButton, Show } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Button>Sign in</Button>
-    </div>
+    <main className="flex min-h-screen items-center justify-center">
+      <Show when="signed-out">
+        <SignInButton>
+          <Button>Sign In</Button>
+        </SignInButton>
+      </Show>
+
+      <Show when="signed-in">
+        <UserButton />
+      </Show>
+    </main>
   );
 }
